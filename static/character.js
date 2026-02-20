e51aeb9c6a2ef6ba38e5fe3cf86e4ca0eb1823c0
bronbron <brmanley@outlook.com>
2026-02-19 19:55:47 -0500

    Update wizard sprite with proper scaling

--- static/character.js
+++ static/character.js
@@ -1,26 +1,44 @@
-export function drawWizard(ctx, x, y, s=4){
-    const p = [
-        "0000222000",
-        "0002444200",
-        "0024444420",
-        "0244444442",
-        "0244DD4442",
-        "0024444420",
-        "0002444200",
-        "0002BB2000",
-        "002BBBBB20",
-        "02B22BB22B",
-        "0222222222"
-    ];
-    for(let j=0;j<p.length;j++){
-        for(let i=0;i<p[j].length;i++){
-            const c = p[j][i];
-            if(c==="0") continue;
-            if(c==="2") ctx.fillStyle="#6a00ff";
-            if(c==="4") ctx.fillStyle="#b48cff";
-            if(c==="D") ctx.fillStyle="#ffe0bd";
-            if(c==="B") ctx.fillStyle="#3b0066";
-            ctx.fillRect(x+i*s, y+j*s, s, s);
-        }
+export function drawWizard(ctx, x, y, scale=4){
+
+const pixels = [
+"000HHHHHH000",
+"00HHHHHHHH00",
+"0HHHHHHHHHH0",
+"0H33333333H0",
+"HH3SSSSSS3HH",
+"H33SSSSSS33H",
+"H3SS0SS0SS3H",
+"0H3SSSSSS3H0",
+"0H03MMMM30H0",
+"0H03BBBB30H0",
+"0H33BBBB33H0",
+"0033BBBB3300",
+"003BBBBBB300",
+"03B33BB33B30",
+"033333333330",
+"003333333300",
+"000333333000"
+];
+
+const colors = {
+"0": null,
+"3": "#6a3dad",   // robe
+"S": "#f1c27d",   // skin
+"M": "#b85c38",   // mouth
+"B": "#3b2a1a",   // beard
+"H": "#f4d03f"    // blonde hair
+};
+
+for(let j=0; j<pixels.length; j++){
+    for(let i=0; i<pixels[j].length; i++){
+        const c = colors[pixels[j][i]];
+        if(!c) continue;
+        ctx.fillStyle = c;
+        ctx.fillRect(
+            Math.floor(x + i*scale),
+            Math.floor(y + j*scale),
+            scale, scale
+        );
     }
+}
 }
