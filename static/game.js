c12c35f75aa566741bb8189721d2e72831010e6e
bronbron <brmanley@outlook.com>
2026-02-20 21:03:52 -0500

    increaaed player speed

--- static/game.js
+++ static/game.js
@@ -14,7 +14,7 @@ let facing={x:1,y:0};
 let camera = { x:0, y:0, targetX:0, targetY:0 };
 const cameraLerp = 0.12;
 
-const moveDelay = 800;
+const moveDelay = 286;
 let lastMove = 0;
 
 /* WALK STATE */
