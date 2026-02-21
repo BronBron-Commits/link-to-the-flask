4ef85a9780a3d80bc5e3343d79874ec6f6f4b240
bronbron <brmanley@outlook.com>
2026-02-21 14:48:20 -0500

    increased tempo of music

--- static/music.js
+++ static/music.js
@@ -7,15 +7,13 @@ export function startMusic(){
   ctx=new (window.AudioContext||window.webkitAudioContext)();
 
   master=ctx.createGain();
-  master.gain.value=0.18; // global volume
+  master.gain.value=0.18;
   master.connect(ctx.destination);
 
   playLoop();
 }
 
-/* ---------- SYNTH VOICE ---------- */
 function playVoice(freq, start, dur){
-
   const osc=ctx.createOscillator();
   const gain=ctx.createGain();
   const filter=ctx.createBiquadFilter();
@@ -28,11 +26,10 @@ function playVoice(freq, start, dur){
 
   osc.frequency.setValueAtTime(freq,start);
 
-  /* legato envelope */
   gain.gain.setValueAtTime(0.0001,start);
-  gain.gain.linearRampToValueAtTime(0.22,start+0.25);      // slow attack
-  gain.gain.linearRampToValueAtTime(0.18,start+dur*0.7);   // sustain
-  gain.gain.linearRampToValueAtTime(0.0001,start+dur+0.9); // long tail
+  gain.gain.linearRampToValueAtTime(0.22,start+0.20);
+  gain.gain.linearRampToValueAtTime(0.18,start+dur*0.65);
+  gain.gain.linearRampToValueAtTime(0.0001,start+dur+0.7);
 
   osc.connect(filter);
   filter.connect(gain);
@@ -42,37 +39,32 @@ function playVoice(freq, start, dur){
   osc.stop(start+dur+1.0);
 }
 
-/* ---------- CHORD PLAYER ---------- */
 function playChord(chord, time, length){
-  for(const f of chord){
-    playVoice(f,time,length);
-  }
+  for(const f of chord) playVoice(f,time,length);
 }
 
-/* ---------- MUSIC LOOP ---------- */
 function playLoop(){
 
-  const bpm=78;
+  const bpm=108;   // FASTER
   const beat=60/bpm;
   const measure=beat*4;
 
-  /* 8 chord progression (warm fantasy) */
   const chords=[
-    [261.63,329.63,392.00,523.25], // C
-    [220.00,261.63,329.63,392.00], // Am
-    [196.00,246.94,329.63,392.00], // G
-    [174.61,220.00,293.66,349.23], // F
-    [196.00,261.63,329.63,392.00], // Gsus
-    [220.00,261.63,329.63,440.00], // Am add9
-    [164.81,246.94,329.63,392.00], // Em
-    [174.61,261.63,349.23,440.00]  // Fmaj7
+    [261.63,329.63,392.00,523.25],
+    [220.00,261.63,329.63,392.00],
+    [196.00,246.94,329.63,392.00],
+    [174.61,220.00,293.66,349.23],
+    [196.00,261.63,329.63,392.00],
+    [220.00,261.63,329.63,440.00],
+    [164.81,246.94,329.63,392.00],
+    [174.61,261.63,349.23,440.00]
   ];
 
   let t=ctx.currentTime+0.2;
 
   function schedule(){
     for(let i=0;i<chords.length;i++){
-      playChord(chords[i],t,measure*1.35); // overlap for legato
+      playChord(chords[i],t,measure*1.2);
       t+=measure;
     }
   }
