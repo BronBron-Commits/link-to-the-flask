1c288deaff35a2aaae3af3a325a8ed16be46b591
Bronson Manley <brmanley@outlook.com>
2026-04-09 12:09:02 -0400

    Enable ProxyFix for forwarded HTTPS headers

--- extensions.py
+++ extensions.py
@@ -7,11 +7,14 @@ Created here to avoid circular imports.
 import os
 from flask import Flask
 from flask_socketio import SocketIO
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(
     __name__,
     root_path=os.path.dirname(os.path.abspath(__file__)),
 )
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 app.config.update(
     SECRET_KEY=os.environ.get("FLASK_SECRET_KEY") or os.environ.get("SECRET_KEY") or "dev-insecure-change-me",
     SESSION_COOKIE_HTTPONLY=True,
